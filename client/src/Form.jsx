0c76db88a032807a5b808beded2ba5bfb17dcb7f
Vincent Chen <vc88086@gmail.com>
2020-03-01 18:13:29 -0500

    implemented form function

--- client/src/Form.jsx
+++ client/src/Form.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 
 function Form() {
-  return <div>Form</div>;
+  return (
+    <div>
+      <form>
+        <h3>Add new cow</h3>
+        <label for="name">Name</label>
+        <input type="text" id="name" name="name" />
+        <label for="description">Description</label>
+        <input type="text" id="description" name="description" />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 }
 export default Form;
